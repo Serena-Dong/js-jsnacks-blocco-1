2a8c38071165a071185afe26fca98fd9601ceb1a
Serena-Dong <serenadong3159@icloud.com>
2022-12-13 16:26:11 +0100

    prompt

---
+++ jsnack1/script.js
@@ -0,0 +1,15 @@
+const result = document.getElementById('result');
+
+//faccio la domanda
+let numbers = [];
+
+numbers[0] = prompt('Dammi un numero');
+numbers[1] = prompt('Dammi un numero');
+numbers[2] = prompt('Dammi un numero');
+
+console.log(numbers);
+
+
+
+
+
