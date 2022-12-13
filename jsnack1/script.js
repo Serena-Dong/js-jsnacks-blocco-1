7cdf52c2b4f0454a1177e8be2dfc84a42824c83d
Serena-Dong <serenadong3159@icloud.com>
2022-12-13 16:36:52 +0100

    jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,14 +1,27 @@
+//collego l'elemento
 const result = document.getElementById('result');
 
-//faccio la domanda
+//faccio le domande
 let numbers = [];
 
 numbers[0] = prompt('Dammi un numero');
 numbers[1] = prompt('Dammi un numero');
 numbers[2] = prompt('Dammi un numero');
+numbers[3] = prompt('Dammi un numero');
+numbers[4] = prompt('Dammi un numero');
+numbers[5] = prompt('Dammi un numero');
+numbers[6] = prompt('Dammi un numero');
+numbers[7] = prompt('Dammi un numero');
+numbers[8] = prompt('Dammi un numero');
+numbers[9] = prompt('Dammi un numero');
 
-console.log(numbers);
-
+//faccio il calcolo
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += +numbers[i];
+}
+//stampo in pagina
+result.innerText=sum;
 
 
 
