b0e1944b097a5940d3ab6a0bc4c1ca47afee1ef2
Serena-Dong <serenadong3159@icloud.com>
2022-12-13 17:06:48 +0100

    creo funzione nel buttone

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,37 @@
-const allowedNames = []
+//aggancio gli elementi
+const nAme = document.getElementById('name')
+const button = document.getElementById('button')
+const result = document.getElementById('result')
+
+
+//lista dei nomi autorizzati
+const allowedNames = [
+    'Gatsby',
+    'Daisy',
+    'Tom',
+    'Nick',
+    'Myrtle'
+]
+
+//verifico l'autorizzazione
+button.addEventListener('click', function(){
+
+    const userName = nAme.value.trim();
+
+    if(!userName){
+        result.innertext= "inserisci il nome";
+        return;
+    }
+
+    let isAllowed = false;
+
+    for (let i=0; !userName && i < allowedNames.length; i++){
+        const currentNames = allowedNames[i];
+
+        if ( currentNames === userName){
+            isAllowed = true;
+        }
+    }
+
+
+})
