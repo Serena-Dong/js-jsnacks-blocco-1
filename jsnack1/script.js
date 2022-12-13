4cd180288f79781712b2d3bbb1c52839713829a6
Serena-Dong <serenadong3159@icloud.com>
2022-12-13 17:35:02 +0100

    ciclo for per la domanda

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,21 +4,15 @@ const result = document.getElementById('result');
 //faccio le domande
 let numbers = [];
 
-numbers[0] = prompt('Dammi un numero');
-numbers[1] = prompt('Dammi un numero');
-numbers[2] = prompt('Dammi un numero');
-numbers[3] = prompt('Dammi un numero');
-numbers[4] = prompt('Dammi un numero');
-numbers[5] = prompt('Dammi un numero');
-numbers[6] = prompt('Dammi un numero');
-numbers[7] = prompt('Dammi un numero');
-numbers[8] = prompt('Dammi un numero');
-numbers[9] = prompt('Dammi un numero');
+
+for (let i = 0; i <= 10; i++){
+    numbers = prompt('Dammi un numero');
+}
 
 //faccio il calcolo
 let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-    sum += +numbers[i];
+for (let w = 0; w < numbers.length; w++) {
+    sum += +numbers[w];
 }
 //stampo in pagina
 result.innerText=sum;
