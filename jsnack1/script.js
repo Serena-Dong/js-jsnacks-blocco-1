e81cec37af9aa40864177cb2d4aaf0d1448d516c
Serena-Dong <serenadong3159@icloud.com>
2022-12-14 15:40:29 +0100

    corretto numero di volte

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,7 +4,7 @@ const result = document.getElementById('result');
 //faccio le domande e il calcolo
 
 let sum = 0;
-for (let i = 0; i < 3; i++){
+for (let i = 0; i < 10; i++){
    const numbers = parseInt(prompt('Dammi un numero').trim());
    sum += numbers;
 }
