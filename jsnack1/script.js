1ba006f254ebafe106d08e976a8f96e5e556a70b
Serena-Dong <serenadong3159@icloud.com>
2022-12-14 15:39:03 +0100

    corretto l'operazione

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,19 +1,14 @@
 //collego l'elemento
 const result = document.getElementById('result');
 
-//faccio le domande
-let numbers = [];
+//faccio le domande e il calcolo
 
-
-for (let i = 0; i <= 10; i++){
-    numbers = prompt('Dammi un numero');
-}
-
-//faccio il calcolo
 let sum = 0;
-for (let w = 0; w < numbers.length; w++) {
-    sum += +numbers[w];
+for (let i = 0; i < 3; i++){
+   const numbers = parseInt(prompt('Dammi un numero').trim());
+   sum += numbers;
 }
+
 //stampo in pagina
 result.innerText=sum;
 
